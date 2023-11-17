80dec4096b078763443c103572f2c462baffafce
JiNT1 <87137040+JiNT1@users.noreply.github.com>
2023-11-17 11:49:55 +0100

    Add files via upload

--- apriltag_gui/include/apriltag_gui/apriltag_gui.h
+++ apriltag_gui/include/apriltag_gui/apriltag_gui.h
@@ -35,29 +35,25 @@ class AprilTagGui{
 		void onReceivedDetectedImage(const apriltag_arm_ros::AprilTagDetectionArrayConstPtr& msg);
 		void onReceivedProb(const std_msgs::Float32MultiArrayConstPtr& msg);
 		void onReceivedEvent(const std_msgs::Int16ConstPtr& msg);
-		void setUpdateValues(int size, bool isSet);
+		void setUpdateValues(int size);
 		//float markedSize(float value, float max, float min);
 		void obtain_target(int value, int buffer);
 		void drawRect(int pos);/*TODO:trasform a range of probabilities into a markedSize value*/
-		void drawAll(int n);
+		void drawAndShow(int numObjs);
 		void obtainNewPos(const apriltag_arm_ros::AprilTagDetectionArrayConstPtr& msg);
 		
 		
 
 	private:
+		//node
 		ros::NodeHandle n;
 		image_transport::ImageTransport image_transport_; // std::shared_ptr<image....> image_transport_
 		
+		//subscriber
 		image_transport::Subscriber sub_image_transport_;
 		ros::Subscriber sub_detected_image;
 		ros::Subscriber sub_prob;
 		ros::Subscriber sub_event_bus;
-		
-		/*
-		message_filters::Subscriber<sensor_msgs::Image> sub_image;
-		message_filters::Subscriber<apriltag_arm_ros::AprilTagDetectionArray> sub_detected_image;
-		message_filters::Subscriber<std_msgs::Float32MultiArray> sub_prob;
-		*/
 		
 		//i topics
 		std::string image_topic;
@@ -67,9 +63,10 @@ class AprilTagGui{
 		
 		//converted image
 		cv::Mat opencv_image;
+
 		//vector of center's positions of each detected apriltags
 		std::vector<cv::Point2d> positions_2d;
-		
+
 		//vector of detected apriltag probabilities		
 		std::vector<float> probs;
 		
@@ -77,7 +74,7 @@ class AprilTagGui{
 		std::vector<float> id_pos;
 		//std::vector<int> id_probs;
 
-		//
+		//code
 		int increasing_code = 500;
 		int start_code = 0;
 		int picking_code = 1000;
@@ -85,26 +82,37 @@ class AprilTagGui{
 		int target_id;
 		int picking_id;
 		int code;
+		/// eliminate an object after picking, can use std::vector::erase()
 
+		//picking mode
 		bool picking;
+		//home mode
+		bool home;
+		//clear position vector after picking
+		bool needClear;
 
 		//OpenCV color channel order is B,G,R
 		// correct = green
 		// wrong = red
 		// target = light blue
 		// other = violet
+		// thickness = 3 to obatin better visualization
 		cv::Scalar correct_color = cv::Scalar(0,255,0);
 		cv::Scalar wrong_color = cv::Scalar(0,0,255);
 		cv::Scalar target_color = cv::Scalar(255,153,51); 
 		cv::Scalar other_color = cv::Scalar(204,0,204);
+		int thickness = 3;
 
+		//default value to draw rectangle with determinate misure in pixel
 		float default_dim_rect = 200.0;
 
 		//number of object detected
 		int n_objects;
 
+		//dinamic use
 		bool continueUpdatePos;
 		
-		bool flag[4] = {false,false,false,false}; //flag[0] for image, flag[1] for center positions, flag[2] probabilities, flag[3] event_bus
+		//flag[0] for image, flag[1] for center positions, flag[2] event_bus
+		bool flag[3] = {false,false,false}; 
 
 };
