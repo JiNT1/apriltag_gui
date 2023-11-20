76591cc550747c08c13bb45cea435217ccccb47a
JiNT1 <87137040+JiNT1@users.noreply.github.com>
2023-11-20 09:24:35 +0100

    Add files via upload

--- apriltag_gui/src/apriltag_gui_node.cpp
+++ apriltag_gui/src/apriltag_gui_node.cpp
@@ -8,20 +8,15 @@ int main(int argc,char **argv){
 	ros::init(argc,argv,"apriltag_gui_node");
 	std::cout<<"ho lanciato il nodo"<<std::endl;
 	AprilTagGui gui;
-	std::cout<<"ho creato un oggetto della classe gui"<<std::endl;
 	if(gui.setup())
 		std::cout<<"tutto bene,sono fuori dal setup"<<std::endl;
 	else
 		std::cout<<"setup fallito"<<std::endl;
-	
 	//ros::Rate rate(100);
 	while(ros::ok()){
 		gui.start();
 		ros::spinOnce();
 		//rate.sleep();
-	}
-	
-	//ros::shutdown();
-	
+	}	
 	return 0;
 }
